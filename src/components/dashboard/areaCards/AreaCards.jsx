fa55a25ded4cf233949942026cd14b6a6b021bbb
Nguyen Xuan Viet <vietnguyenxuan3112@gmail.com>
2024-10-06 10:59:01 +0700

    init

---
+++ src/components/dashboard/areaCards/AreaCards.jsx
@@ -0,0 +1,51 @@
+import AreaCard from "./AreaCard";
+import "./AreaCards.scss";
+
+const AreaCards = () => {
+  return (
+    <section className="content-area-cards">
+      <AreaCard
+        colors={["#e4e8ef", "#475be8"]}
+        percentFillValue={80}
+        cardInfo={{
+          title: "Lượt nâng cấp",
+          value: "120",
+        }}
+      />
+      <AreaCard
+        colors={["#e4e8ef", "#4ce13f"]}
+        percentFillValue={50}
+        cardInfo={{
+          title: "Doanh thu",
+          value: "$8.2K",
+        }}
+      />
+      <AreaCard
+        colors={["#e4e8ef", "#f29a2e"]}
+        percentFillValue={40}
+        cardInfo={{
+          title: "Khách hàng mới",
+          value: "154",
+        }}
+      />
+      <AreaCard
+        colors={["#e4e8ef", "#f29a2e"]}
+        percentFillValue={40}
+        cardInfo={{
+          title: "Xu hướng bệnh",
+          value: "Gout",
+        }}
+      />
+      <AreaCard
+        colors={["#e4e8ef", "#f29a2e"]}
+        percentFillValue={40}
+        cardInfo={{
+          title: "Tổng số thực đơn",
+          value: "2935",
+        }}
+      />
+    </section>
+  );
+};
+
+export default AreaCards;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,130 @@
+import { useContext, useEffect, useRef } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
+import { LIGHT_THEME } from "../../constants/themeConstants";
+import WeCareLogo from "../../assets/images/wecare_logo_size36.png"; // Thêm đường dẫn tới ảnh mới
+import { 
+  MdPerson, 
+  MdLocalHospital, 
+  MdRestaurantMenu, 
+  MdCategory,
+  MdOutlineClose, 
+  MdOutlineGridView, 
+  MdOutlineSettings, 
+  MdOutlineLogout 
+} from "react-icons/md";
+import { Link } from "react-router-dom";
+import "./Sidebar.scss";
+import { SidebarContext } from "../../context/SidebarContext";
+
+const Sidebar = () => {
+  const { theme } = useContext(ThemeContext);
+  const { isSidebarOpen, closeSidebar } = useContext(SidebarContext);
+  const navbarRef = useRef(null);
+
+  // closing the navbar when clicked outside the sidebar area
+  const handleClickOutside = (event) => {
+    if (
+      navbarRef.current &&
+      !navbarRef.current.contains(event.target) &&
+      event.target.className !== "sidebar-oepn-btn"
+    ) {
+      closeSidebar();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
+      ref={navbarRef}
+    >
+      <div className="sidebar-top">
+        <div className="sidebar-brand">
+          <img src={WeCareLogo} alt="WeCare Logo" />
+          <span className="sidebar-brand-text">
+            <span className="brand-we">We</span>
+            <span className="brand-care">care</span>
+          </span>
+        </div>
+        <button className="sidebar-close-btn" onClick={closeSidebar}>
+          <MdOutlineClose size={24} />
+        </button>
+      </div>
+      <div className="sidebar-body">
+        <div className="sidebar-menu">
+          <ul className="menu-list">
+            <li className="menu-item">
+              <Link to="/" className="menu-link active">
+                <span className="menu-link-icon">
+                  <MdOutlineGridView size={18} />
+                </span>
+                <span className="menu-link-text">Trang chủ</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdPerson size={20} />
+                </span>
+                <span className="menu-link-text">Người dùng</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdLocalHospital size={20} />
+                </span>
+                <span className="menu-link-text">Chuyên gia dinh dưỡng</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdRestaurantMenu size={20} />
+                </span>
+                <span className="menu-link-text">Menu</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdCategory size={20} />
+                </span>
+                <span className="menu-link-text">Danh mục</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebar-menu sidebar-menu2">
+          <ul className="menu-list">
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdOutlineSettings size={20} />
+                </span>
+                <span className="menu-link-text">Settings</span>
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                <span className="menu-link-icon">
+                  <MdOutlineLogout size={20} />
+                </span>
+                <span className="menu-link-text">Logout</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
