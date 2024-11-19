31b3beda9316a6d22eda90cf2b11fea48bedc7ce
Nguyen Xuan Viet <vietnguyenxuan3112@gmail.com>
2024-11-19 12:14:39 +0700

    update

--- src/screens/login/Login.jsx
+++ src/screens/login/Login.jsx
@@ -23,7 +23,7 @@ const Login = ({ setIsLoggedIn, setUserRole }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://wecareexe201.azurewebsites.net/api/user/login", {
+      const response = await fetch("https://midge-noted-tuna.ngrok-free.app/api/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
